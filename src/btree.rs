8a16260cacfbc1645fc3b336a6d285f22520fdcd
Git-Math <matoo3@hotmail.fr>
2017-03-01 19:42:22 +0100

    btree: use list of root to handle parenthesis

--- src/btree.rs
+++ src/btree.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::collections::LinkedList;
 
 struct BTreeNode {
     v: u8,
@@ -8,16 +9,20 @@ struct BTreeNode {
 }
 
 impl BTreeNode {
-    pub fn insert(mut self, token: String, value: u8) -> BTreeNode {
-        match self.v.cmp(&value) {
+	fn new(token: &str, value: u8) -> BTreeNode {
+		BTreeNode { t: token.to_string(), v: value, l: None, r: None }
+	}
+
+    pub fn insert(mut self, new_node: BTreeNode) -> BTreeNode {
+        match self.v.cmp(&new_node.v) {
             Ordering::Less |
             Ordering::Equal => {
                 assert!((self.l.is_some() && self.r.is_some()) ||
-                        (self.v == 1 && value != 1),
-                        "Unexpected token near '{}': '{}'", self.t, token);
+                        (self.v == 1 && new_node.v != 1),
+                        "Unexpected token near '{}': '{}'", self.t, new_node.t);
                 BTreeNode {
-                    v: value,
-                    t: token,
+                    v: new_node.v,
+                    t: new_node.t,
                     l: Some(Box::new(self)),
                     r: None
                 }
@@ -25,12 +30,12 @@ impl BTreeNode {
             Ordering::Greater => {
                 self.r = match self.r {
                     Some(node) => {
-                        Some(Box::new(node.insert(token, value)))
+                        Some(Box::new(node.insert(new_node)))
                     },
                     None => {
-                        assert!(value == 1,
-                                "Unexpected token near '{}': '{}'", self.t, token);
-                        Some(Box::new(BTreeNode { v: value, t: token, l: None, r: None }))
+                        assert!(new_node.v <= 1,
+                                "Unexpected token near '{}': '{}'", self.t, new_node.t);
+                        Some(Box::new(new_node))
                     }
                 };
                 self
@@ -42,64 +47,117 @@ impl BTreeNode {
         if let Some(ref left) = self.l {
             left.display(&self.t);
         }
+		print!("{}", self.t);
         if let Some(ref right) = self.r {
             right.display(&self.t);
         }
-        if *prev_token != self.t {
-            print!("{}", self.t);
-        }
+//        if *prev_token != self.t {
+//          print!("{}", self.t);
+// }
     }
-
 }   
 
 struct BTree {
-    root: Option<BTreeNode>
+	root_list: LinkedList<Option<BTreeNode>>
 }
 
 impl BTree {
+	fn new() -> BTree {
+		BTree { root_list: LinkedList::new() }
+	}
+
     pub fn insert(&mut self, token: &str) {
+		match token {
+			"(" => self.root_list.push_front(None),
+			")" => {
+				let mut arthur_root = self.root_list.pop_front().unwrap().unwrap();
 
-        let old_root = self.root.take();
+				arthur_root.v = 0;
 
-        let value = match token {
-            "=>" => 5,
-            "|"  => 4,
-            "^"  => 4,
-            "+"  => 3,
-            _    => 1
-        };
+				if let Some(target_option) = self.root_list.pop_front() {
+						let mut target = target_option.unwrap();
+						println!("bonjour arthur");
+						target = target.insert(arthur_root);
+						self.root_list.push_front(Some(target));
+				}
+				else {
+					self.root_list.push_front(Some(arthur_root));
+				}
+			},
+			_ => {
+				let old_root = self.root_list.pop_front();
 
-        let new_root =
-            match old_root {
-                Some(root) => root.insert(token.to_string(), value),
-                None => {
-                    assert!(value == 1, "Unexpected token: '{}'", token);
-                    BTreeNode { v: value, t: token.to_string(), l: None, r: None }
-                }
-            };
+				let value = match token {
+					"=>" => 5,
+					"|"  => 4,
+					"^"  => 4,
+					"+"  => 3,
+					_    => 1
+				};
 
-        self.root = Some(new_root);
-    }
+				let node = BTreeNode::new(token, value);
+				let new_root =
+				match old_root {
+					Some(root) => match root {
+						Some(root) => root.insert(node),
+						None => {
+							assert!(value == 1, "Unexpected token: '{}'", token);
+							node
+						}
+					},
+					None => {
+						assert!(value == 1, "Unexpected token: '{}'", token);
+						node
+					}
+				};
+
+				self.root_list.push_front(Some(new_root));
+			}
+		}
+	}
 
     pub fn display(&self) {
-        if let Some(ref root) = self.root {
-            root.display(&"".to_string());
+        if let Some(root_option) = self.root_list.front() {
+				if let Some(root) = root_option.as_ref() {
+						root.display(&"".to_string());
+					println!();
+				}
         }
     }
 }
 
 fn main() {
-    let mut tree = BTree { root: None };
+    let mut tree = BTree::new();
 
     tree.insert("A");
-    tree.insert("+");
+    tree.insert("|");
     tree.insert("B");
     tree.insert("+");
+	tree.insert("(");
+    tree.insert("F");
+    tree.insert("|");
+    tree.insert("G");
+	tree.insert(")");
+	tree.insert("+");
     tree.insert("C");
     tree.insert("|");
     tree.insert("D");
     tree.insert("=>");
     tree.insert("E");
+
+/*
+	tree.insert("(");
+    tree.insert("F");
+    tree.insert("|");
+    tree.insert("G");
+    tree.insert(")");
+    tree.insert("+");
+    tree.insert("C");
+    tree.insert("|");
+    tree.insert("D");
+    tree.insert("=>");
+	tree.insert("E");
+*/
 
     tree.display();
     tree.display();
